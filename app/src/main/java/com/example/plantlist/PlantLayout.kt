428ee0adc58b951d5e8c4eefab6957452d3186f7
Joshua <joshuagutierrez724@gmail.com>
2023-06-23 08:54:57 -0400

    Theme and shapes added

--- app/src/main/java/com/example/plantlist/PlantLayout.kt
+++ app/src/main/java/com/example/plantlist/PlantLayout.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -12,12 +14,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.plantlist.model.DataSource
 import com.example.plantlist.model.PlantItem
 import com.example.plantlist.ui.theme.PlantListTheme
+
 
 @Composable
 fun PlantCard (
@@ -32,14 +38,15 @@ fun PlantCard (
 
     ){
         Column(
-
+           modifier.padding(dimensionResource(R.dimen.large_padding))
         ){
             Text(
                 text = stringResource(plantItem.plantNameId)
             )
             Image(
                 painter = painterResource(plantItem.plantImageId),
-                contentDescription = null
+                contentDescription = null,
+                modifier.clip(MaterialTheme.shapes.large)
             )
             if(isExpanded){
                 Text(
---
+++ app/src/main/java/com/example/plantlist/ui/theme/Shape.kt
@@ -0,0 +1,16 @@
+package com.example.plantlist.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    large = RoundedCornerShape(
+        topEnd = 16.dp,
+        bottomStart = 16.dp,
+        bottomEnd = 0.dp,
+        topStart = 0.dp
+    )
+
+)
